c7b6b4d906b5784377caa136e772673e463f3700
Steven Costello <arrow816@gmail.com>
2024-05-08 20:34:18 -0400

    Update GHP to work with new code.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,25 @@
 jQuery(document).ready(function() {
+
+	$.each($(".code_load"), function(i,item) {
+		var load_src = $(item).data("cssrc");
+		console.log(item, load_src);
+		fetch(load_src).then(function (response) {
+			if (response.ok) {
+				return response.text();
+			}
+			throw response;
+		}).then(function (text) {
+			item.innerHTML = escapeHtml(text);
+		});
+	});
+
+	// $("#main-content").load("snippets/about-snippet.html", function(responseTxt, statusTxt, jqXHR){
+    //     if(statusTxt == "error"){
+    //         alert("Error: " + jqXHR.status + " " + jqXHR.statusText);
+    //     }
+    // });
+
+
     
 	$.each($(".date_expire"), function(i,item) {
 	  //logic to get date, and determin if current date is before or after set date
@@ -6,28 +27,40 @@ jQuery(document).ready(function() {
 	  var currentDate = new Date();
 	  var date_check = check_field_date(currentDate, fieldDate);
 
-	  $.each($(".date_expire_action"), function(ii,date_expire_action_item) {
-		  var expire_action_data_action = $(date_expire_action_item).data('action');
-		  switch (expire_action_data_action) {
-			  case "hide_af":
-				  if(!date_check){
-					  item_action($(date_expire_action_item),"show");
-				  }else{
-					  item_action($(date_expire_action_item),"hide");
-				  }
-				  
-				  break;
-			  case "show_af":
-				  if(date_check){
-					  item_action($(date_expire_action_item),"show");
-				  }else{
-					  item_action($(date_expire_action_item),"hide");
-				  }
-				  break;
-			  default:
-				  break;
+
+	  var parent_hide = $(item).data('hideparent');
+	  //if parent_hide is set and == true, hide whole block after date, else check for sub items to show or hide after date
+	  if (parent_hide !== undefined && parent_hide == true){
+		  console.log(parent_hide, date_check);
+		  if(!date_check){
+			  item_action($(item),"show");
+		  }else{
+			  item_action($(item),"hide");
 		  }
-	  });
+	  }else{
+		  $.each($(".date_expire_action"), function(ii,date_expire_action_item) {
+			  var expire_action_data_action = $(date_expire_action_item).data('action');
+			  switch (expire_action_data_action) {
+				  case "hide_af":
+					  if(!date_check){
+						  item_action($(date_expire_action_item),"show");
+					  }else{
+						  item_action($(date_expire_action_item),"hide");
+					  }
+					  
+					  break;
+				  case "show_af":
+					  if(date_check){
+						  item_action($(date_expire_action_item),"show");
+					  }else{
+						  item_action($(date_expire_action_item),"hide");
+					  }
+					  break;
+				  default:
+					  break;
+			  }
+		  });
+	  }
 	});
 });
 
@@ -65,6 +98,25 @@ function check_field_date(currentDate, fieldDate){
 function item_action(the_item,the_action){
   if(the_action == "show"){
 	  $(the_item).show();
+  }else if(the_action == "hide"){
+	  $(the_item).hide();
   }
 
 }
+
+function get_code_snippet(load_url,the_item){
+	var test = fetch(load_url)
+		.then(response=> response.text())
+		.then(text=> document.getElementById('elementID').innerHTML = text);
+	console.log(test);
+}
+
+function escapeHtml(unsafe)
+{
+    return unsafe
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&#039;");
+ }
